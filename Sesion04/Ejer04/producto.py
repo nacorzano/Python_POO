97118a1bbf12518619cc2ee7f22e527e117d498e
nacorzano <nacorzano@gmail.com>
2026-06-12 13:07:10 +0200

    Update producto.py

--- Sesion04/Ejer04/producto.py
+++ Sesion04/Ejer04/producto.py
@@ -9,13 +9,28 @@ metodo de clase -->
 import subprocess
 class Producto:
     total = 0
+
     def __init__(self, codigo, nombre, precio, stock):
         self.codigo = codigo
-        self.nombre = nombre
+        self.nombre = nombre        
         self.precio = precio
         self.stock = stock
-        total+=1
+        Producto.total+=1
 
+    def __str__(self):
+        return f"\nCodigo: {self.codigo}\nNombre: {self.nombre}\nPrecio: {self.precio}\nStock: {self.stock}\n\n" 
+
+    def stock_total(prod):
+        x = 0
+        for i in prod:
+            x += i.stock
+        return x
+
+    def valor_total(prod):
+        x = 0
+        for i in prod:
+            x += (i.precio * i.stock)
+        return x
 
     def limpiar_pantalla():
        subprocess.run('cls', shell=True)
